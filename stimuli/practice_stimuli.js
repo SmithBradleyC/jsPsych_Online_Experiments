20c3833c9191d697ddb4246cee2f5b8c5708998c
BradleySmithC <bradley.carlyle.smith@gmail.com>
2020-04-08 12:55:48 -0500

    making_contingency_practice_experiment

---
+++ stimuli/practice_stimuli.js
@@ -0,0 +1,122 @@
+var practice_stimuli = [
+{
+	"stimulus":"<p id = 'contingency_stim', style = 'color: red; font-size: 60pt;'>MONTH</p>", 
+	"data": {
+		"word": "MONTH",
+		 "colour": "red",
+		 "cor_response": "j",
+		 "phase": "Practice",
+		 "stim_type": "Practice",
+	},
+},
+{
+	"stimulus":"<p id = 'contingency_stim', style = 'color: yellow; font-size: 60pt;'>MONTH</p>", 
+	"data": {
+		"word": "MONTH",
+		 "colour": "yellow",
+		 "cor_response": "k",
+		 "phase": "Practice",
+		 "stim_type": "Practice",
+	},
+},
+{
+	"stimulus":"<p id = 'contingency_stim', style = 'color: green; font-size: 60pt;'>MONTH</p>", 
+	"data": {
+		"word": "MONTH",
+		 "colour": "green",
+		 "cor_response": "l",
+		 "phase": "Practice",
+		 "stim_type": "Practice",
+	},
+},
+{
+	"stimulus":"<p id = 'contingency_stim', style = 'color: red; font-size: 60pt;'>UNDER</p>", 
+	"data": {
+		"word": "UNDER",
+		 "colour": "red",
+		 "cor_response": "j",
+		 "phase": "Practice",
+		 "stim_type": "Practice",
+	},
+},
+{
+	"stimulus":"<p id = 'contingency_stim', style = 'color: yellow; font-size: 60pt;'>UNDER</p>", 
+	"data": {
+		"word": "UNDER",
+		 "colour": "yellow",
+		 "cor_response": "k",
+		 "phase": "Practice",
+		 "stim_type": "Practice",
+	},
+},
+{
+	"stimulus":"<p id = 'contingency_stim', style = 'color: green; font-size: 60pt;'>UNDER</p>", 
+	"data": {
+		"word": "UNDER",
+		 "colour": "green",
+		 "cor_response": "l",
+		 "phase": "Practice",
+		 "stim_type": "Practice",
+	},
+},
+{
+	"stimulus":"<p id = 'contingency_stim', style = 'color: red; font-size: 60pt;'>PLATE</p>", 
+	"data": {
+		"word": "PLATE",
+		 "colour": "red",
+		 "cor_response": "j",
+		 "phase": "Practice",
+		 "stim_type": "Practice",
+	},
+},
+{
+	"stimulus":"<p id = 'contingency_stim', style = 'color: yellow; font-size: 60pt;'>PLATE</p>", 
+	"data": {
+		"word": "PLATE",
+		 "colour": "yellow",
+		 "cor_response": "k",
+		 "phase": "Practice",
+		 "stim_type": "Practice",
+	},
+},
+{
+	"stimulus":"<p id = 'contingency_stim', style = 'color: green; font-size: 60pt;'>PLATE</p>", 
+	"data": {
+		"word": "PLATE",
+		 "colour": "green",
+		 "cor_response": "l",
+		 "phase": "Practice",
+		 "stim_type": "Practice",
+	},
+},
+{
+	"stimulus":"<p id = 'contingency_stim', style = 'color: red; font-size: 60pt;'>CLOCK</p>", 
+	"data": {
+		"word": "CLOCK",
+		 "colour": "red",
+		 "cor_response": "j",
+		 "phase": "Practice",
+		 "stim_type": "Practice",
+	},
+},
+{
+	"stimulus":"<p id = 'contingency_stim', style = 'color: yellow; font-size: 60pt;'>CLOCK</p>", 
+	"data": {
+		"word": "CLOCK",
+		 "colour": "yellow",
+		 "cor_response": "k",
+		 "phase": "Practice",
+		 "stim_type": "Practice",
+	},
+},
+{
+	"stimulus":"<p id = 'contingency_stim', style = 'color: green; font-size: 60pt;'>CLOCK</p>", 
+	"data": {
+		"word": "CLOCK",
+		 "colour": "green",
+		 "cor_response": "l",
+		 "phase": "Practice",
+		 "stim_type": "Practice",
+	},
+},
+]
