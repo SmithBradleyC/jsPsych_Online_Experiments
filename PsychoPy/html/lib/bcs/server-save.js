ef74dd3e46d3f583cd917d1db9e62611a1fe71e5
BradleySmithC <bradley.carlyle.smith@gmail.com>
2020-10-14 12:15:53 -0500

    remove_PsychoPy_stuff

--- PsychoPy/html/lib/bcs/server-save.js
+++
@@ -1,8 +0,0 @@
-function saveData(name, data, type) {
-	const blob = new Blob([data], { type });
-	var xhr = new XMLHttpRequest(); // point to right place
-  xhr.open('POST', 'write_data.php'); // 'write_data.php' is the path to the php file // open php file
-  xhr.setRequestHeader('Content-Type', 'application/json'); // sending a JSON
-  xhr.send(JSON.stringify({filename: name, filedata: data})); // save the data to the data folder. Will be a csv
-}
-export { saveData };
